6cc8377d5041e50cb64c9fea1dab723855534c4f
John Dana <john@danasd.com>
2024-03-12 13:50:24 -0600

    Experiment with ButtonGroup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 export default function App() {
-
   return (
-    <>  
+    <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
           <PhotoCamera />
-          <Typography variant="h6">
-            Photo Album
-          </Typography>
+          <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
       <main>
         <div>
           <Container maxWidth="sm">
-            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Photo Album
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Hello everyone, this is a photo album and I'm trying to make this sentence as long as possible so we can see how it looks on the screen.
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone, this is a photo album and I'm trying to make this
+              sentence as long as possible so we can see how it looks on the
+              screen.
             </Typography>
           </Container>
         </div>
+        <ButtonGroup variant="contained" aria-label="Basic button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
       </main>
     </>
-
   );
 }
